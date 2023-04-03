1d1e070bad3fe111ba8beed5ef04dacc7c9a4714
Lorena <77021882+loresgarcia@users.noreply.github.com>
2023-04-03 16:06:37 -0300

    add: logo

---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,7 @@
+const Logo = () => {
+    return (
+        <img src="imagens/logo-menor.png" alt="logo da dartsiders" />
+    )
+}
+
+export default Logo;
--- src/componentes/MenuLateral/index.js
+++ src/componentes/MenuLateral/index.js
@@ -1,6 +1,26 @@
+import Logo from "../Logo";
+
 const MenuLateral = () => {
     return (
-        <>Olá mundo!</>
+        <section>
+            <Logo />
+            <img src="https://github.com/loresgarcia.png" alt="foto de perfil admin" />
+            <p>Lorena Garcia</p>
+            <span>Admin</span>
+
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Curso</li>
+                    <li>Estudantes</li>
+                    <li>Pagamentos</li>
+                    <li>Relatórios</li>
+                    <li>Configurações</li>
+                </ul>
+            </nav>
+
+            <button>Logout</button>
+        </section>
     )
 }
 
