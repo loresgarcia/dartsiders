18adb9da1eea88cc082d8ab377ec911523e82f80
Lorena <77021882+loresgarcia@users.noreply.github.com>
2023-06-21 18:24:54 -0300

    add: estrutura do novo estudante

--- src/componentes/CampoFormulario/index.js
+++ src/componentes/CampoFormulario/index.js
@@ -1,10 +1,24 @@
 import "./CampoFormulario.css";
 
-const CampoFormulario = ({titulo, tipo, placeholder}) => {
+const CampoFormulario = ({titulo, tipo, placeholder, valor, chave, novoEstudante, setNovoEstudante}) => {
+    const teste = (e) => {
+        setNovoEstudante({
+            ...novoEstudante,
+            [chave]: e.target.value
+        })
+    }
+    
     return (
         <label className="campo">
             {titulo}
-            <input className="campo__input" type={tipo} placeholder={placeholder} />
+            <input 
+                className="campo__input" 
+                type={tipo} 
+                placeholder={placeholder} 
+                value={valor} 
+                onChange={(e) => teste(e)}
+                required 
+            />
         </label>
     )
 }
--- src/componentes/ConteudoNovoEstudante/index.js
+++ src/componentes/ConteudoNovoEstudante/index.js
@@ -1,19 +1,45 @@
+import { useState } from "react";
+
+// import axios from "axios";
+
 import CampoFormulario from "../CampoFormulario";
 
 import "./ConteudoNovoEstudante.css";
 
 const ConteudoNovoEstudante = () => {
+    const dataAtual = new Date()
+    
+    const dia = dataAtual.getDate().toString().padStart(2, '0');
+    const mes = (dataAtual.getMonth() + 1).toString().padStart(2, '0');
+    const ano = dataAtual.getFullYear()
+
+    const dataFormatada = `${ano}-${mes}-${dia}`
+
+    const [novoEstudante, setNovoEstudante] = useState({
+        foto: "",
+        nome: "",
+        telefone: "",
+        email: "",
+        matricula: "",
+        admissao: dataFormatada,
+    })
+
+    const enviarNovoEstudante = () => {
+        // axios.post("http://localhost:3000/dados",)
+        console.log(novoEstudante)
+    }
+
     return (
         <div className="novo-estudante">
             <h3 className="novo-estudante__titulo">Novo estudante</h3>
             <form className="novo-estudante__formulario">
-                <CampoFormulario titulo="Foto" tipo="file" />
-                <CampoFormulario titulo="Nome" tipo="text" placeholder="Nome completo" />
-                <CampoFormulario titulo="Telefone" tipo="tel" placeholder="Telefone" />
-                <CampoFormulario titulo="E-mail" tipo="email" placeholder="E-mail" />
-                <CampoFormulario titulo="Matrícula" tipo="text" placeholder="Matrícula" />
-                <CampoFormulario titulo="Admissão" tipo="date" />
-                <button className="novo-estudante__botao">Adicionar estudante</button>
+                <CampoFormulario novoEstudante={novoEstudante} setNovoEstudante={setNovoEstudante} chave="foto" valor={novoEstudante.foto} titulo="Foto" tipo="text" />
+                <CampoFormulario novoEstudante={novoEstudante} setNovoEstudante={setNovoEstudante} chave="nome" valor={novoEstudante.nome} titulo="Nome" tipo="text" placeholder="Nome completo" />
+                <CampoFormulario novoEstudante={novoEstudante} setNovoEstudante={setNovoEstudante} chave="telefone" valor={novoEstudante.telefone} titulo="Telefone" tipo="tel" placeholder="Telefone" />
+                <CampoFormulario novoEstudante={novoEstudante} setNovoEstudante={setNovoEstudante} chave="email" valor={novoEstudante.email} titulo="E-mail" tipo="email" placeholder="E-mail" />
+                <CampoFormulario novoEstudante={novoEstudante} setNovoEstudante={setNovoEstudante} chave="matricula" valor={novoEstudante.matricula} titulo="Matrícula" tipo="text" placeholder="Matrícula" />
+                <CampoFormulario novoEstudante={novoEstudante} setNovoEstudante={setNovoEstudante} chave="admissao" valor={novoEstudante.admissao} titulo="Admissão" tipo="date" />
+                <button className="novo-estudante__botao" onClick={() => enviarNovoEstudante()}>Adicionar estudante</button>
             </form>
         </div>
     )
